fb8a6c088254c74c0f40195a6969f7ee4fa63716
lotositsh <lotositsh@gmail.com>
2023-04-02 04:48:50 +0300

    1

--- med.py
+++ med.py
@@ -6,10 +6,12 @@ import random
 filename = "melody1.mp3"
 duration = 1800
 interval = random.randint(30, 60)
+print(interval)
 filename2 = "melody2.mp3"
 
+
 def assan(sig, frame):
-    print("1 - shavasana; 0 - exit")
+    print("1 - shavasana;\n0 - exit")
     choice = input("Enter your choice: ")
     if choice == "1":
         os.system("play " + filename2)
@@ -22,9 +24,8 @@ signal.signal(signal.SIGINT, assan)
 # Start the countdown loop
 os.system("termux-wake-lock")
 remaining_time = duration
-while remaining_time > 0:
-    print(f"{remaining_time // 60} minutes remaining...")
+for i in range(duration, 0, -interval):
+    print(f"{i//60} minutes remaining...")
     os.system("play " + filename)
-    time.sleep(min(interval, remaining_time))
-    remaining_time -= interval
+    time.sleep(interval)
 os.system("termux-wake-unlock")
